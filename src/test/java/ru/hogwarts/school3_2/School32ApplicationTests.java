9f5d49e26710d440c5a0a8129784075d196fa39d
Lexey9105 <umpp120@gmail.com>
2023-08-21 00:54:27 +0300

    Сделано дз 3.2

---
+++ src/test/java/ru/hogwarts/school3_2/School32ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class School32ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
